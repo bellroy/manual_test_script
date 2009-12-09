349cf544abbd57938060c8df440164437c4da33d
Thomas Sommer <tom@toms-macbook-pro.local>
2009-12-10 09:03:17 +1100

    Cleaned up the parser. Current limitation: We can only have a maximum of 3 levels.
    Conflicts:
    
    	lib/manual_test_script.rb
    	lib/test_script.rb
    	lib/test_script.treetop

--- lib/manual_test_script.rb
+++ lib/manual_test_script.rb
@@ -1,38 +1,29 @@
 require 'highline/import'
+require 'test_script'
 
 class ManualTestScript
   class << self
     # "Section header" => {
-    #   :answer => nil,
-    #     :subs => {
-    #       "Question prerequisite (or standalone question)" => {
-    #         :answer => nil,
-    #         :subs => {
-    #           "Question prerequisite (or standalone question)" => {
-    #             :answer => true,
-    #             :subs => nil
-    #           }
-    #         }
-    #       }
-    #     }
+    #   "Question prerequisite (or standalone question)" => {
+    #     "Question prerequisite (or standalone question)" => { }
     #   }
     # }
-    
+
     attr_accessor :intro
 
     def run
       @tests = ActiveSupport::OrderedHash.new
       @test_stack = []
       @failing_tests = []
-      
+
       puts "Welcome to the manual testing script."
       puts "Please \e[1mdo not skip through sections of this test\e[0m, it is as long as it needs to be.", $/
 
       intro.call if intro
 
       puts $/, "\e[32m\e[1mPlease confirm the following:\e[0m"
-      
-      load_tests
+
+      @tests = TestScriptParser.new.parse(File.read("#{Rails.root}/spec/manual_testing.txt")).collect
       run_tests
     ensure
       if @failing_tests.empty?
@@ -52,16 +43,16 @@ class ManualTestScript
 
     def run_tests(tests = @tests)
       @test_stack.push nil
-      tests.each do |test, details|
+      tests.each do |test, sub_tests|
         @test_stack[-1] = test
         print ' ' * (@test_stack.length - 1)
-        if details[:subs].present?
+        if sub_tests && !sub_tests.empty?
           if @test_stack.length == 1
             puts $/, "\e[1m#{test}\e[0m", '-' * test.length
           else
             puts test
           end
-          run_tests(details[:subs])
+          run_tests(sub_tests)
         else
           case ask(test)
           when 'y': # Good! continue...
@@ -71,51 +62,6 @@ class ManualTestScript
         end
       end
       @test_stack.pop
-    end
-
-    def load_tests
-      File.open("#{Rails.root}/spec/manual_testing.txt") do |file|
-        file.each_line do |line|
-          case line
-          when /^- (.+)/ # Section header
-            @current_section = $1
-            @tests[@current_section] = { :subs => ActiveSupport::OrderedHash.new }
-
-            @test_stack.clear
-            @test_stack.push @current_section
-          when /(^-{2,})|(^\s*$)/ # Separator or blank line, ignored
-            next
-          when /^(\s*)(.*)$/ # Test or test precondition
-            raise "Test outside of section on line #{file.lineno}." unless @current_section
-
-            test = $2
-            case jump = ($1.length + 2) / 2 - (@test_stack.length - 1)
-            when -1 # have jumped out one level
-              @test_stack.pop
-              @test_stack[-1] = test
-            when 0 # at the same level, no nothing
-              @test_stack[-1] = test
-            when 1 # have gone deeper into the stack
-              @test_stack.push test
-            else
-              raise "Indentation error near line #{file.lineno}." if jump > 1
-              jump.abs.times { @test_stack.pop }
-              @test_stack[-1] = test
-            end
-
-            loc = @tests
-            @test_stack[0...-1].each do |t|
-              loc = loc[t][:subs] ||= ActiveSupport::OrderedHash.new
-            end
-
-            loc[test] = {}
-          else
-            raise "Scheisse!"
-          end
-        end
-      end
-
-      @test_stack.clear
     end
 
     def ask(question)
---
+++ lib/tests_node.rb
@@ -0,0 +1,13 @@
+module TestsNode
+  def collect
+    level = case
+              when test.elements.first.respond_to?(:tests_a): :a
+              when test.elements.first.respond_to?(:tests_b): :b
+              else                                            :c
+            end
+
+    test_hash = ActiveSupport::OrderedHash.new
+    test.elements.each { |t| test_hash[t.name.text_value] = t.send("tests_#{level}").collect }
+    test_hash
+  end
+end
